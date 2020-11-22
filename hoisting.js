7a7897f0fae0dd518b927884ac76133b3052d01f
Amanda Nikrant <ajnikrant@gmail.com>
2020-11-22 21:35:26 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,12 +10,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
 }
+  thisIsCrazy();
+}
+crazy();
 
 function sayMyName() {
   // fix the code in here:
